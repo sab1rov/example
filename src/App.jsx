a663813602c7a8a19d153914cdeca8b43d40a626
unknown <zohid.zokirivich2000@gmail.com>
2024-10-22 11:57:45 +0500

    added: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import { routes } from "./constants/routes";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="main">
         <Routes>
           {routes.map((item) => (
             <Route path={item.path} element={item.element} key={item.id} />
           ))}
+          <Route path="*" element={<About />} />
         </Routes>
-      </main>
     </>
   );
 }
